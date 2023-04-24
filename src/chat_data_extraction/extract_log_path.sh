5e7d2e77855ddfda550c9e403e739fc79e0cb9bc
niels <niels.tilch.1@gmail.com>
2023-04-25 00:51:29 +0200

    Extract .minecraft logs for data mining (Windows only)

---
+++ src/chat_data_extraction/extract_log_path.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+project_dir=$(pwd)'/logs_data/log_paths.txt'
+
+cd C:\Users
+cd $(find -path './*/AppData/Roaming/.minecraft' ! -path './Default/*' -print -quit )
+cd logs
+
+realpath $(find -name '*.log.gz')
+realpath $(find -name '*.log.gz') > $project_dir
