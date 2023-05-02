46af74313305a57c07df4266b3b39942ea30c24f
niels <niels.tilch.1@gmail.com>
2023-05-02 14:35:45 +0200

    Map transfer to .mincraft saves script

---
+++ src/external_data/maps/attributes/map_transfer/map_tranfer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+file=$(pwd)'/file.txt'
+
+cd ./../../../..
+source=$(pwd)
+echo $source
+python -m external_data.maps.attributes.map_transfer.map_transfer
+echo 'Getting top map done'
+
+cd C:\Users
+
+mc_file=$(find -path './*/AppData/Roaming/.minecraft' ! -path './Default/*' -print -quit )
+mc_file="${mc_file:1}"
+mc_path=$(pwd)$mc_file
+
+cd $mc_path
+rm -r saves
+mkdir saves
+
+cd C:\Users
+
+track=0
+while read p; do
+
+  if [ $((track%2)) == 0 ]; then
+    map=$source$p
+    map=${map::-1}
+    cp -r $map $mc_path'/saves'
+  else
+    echo $p
+  fi
+  ((track++))
+
+done<$file
+
+
+echo 'Done'
+exit 0
---
+++ src/external_data/maps/attributes/map_transfer/map_transfer.py
@@ -0,0 +1,21 @@
+import util.main.database as db
+
+top = int(input('Number of map for the transfer : '))
+mode = str(input("Specific gamemode filter (keep blank if you don't want it) : "))
+
+# Connection to database
+conn = db.create_connection()
+
+if mode == "":
+    df_path = db.top_map_to_df(conn=conn, top=top)
+else:
+    df_path = db.specific_top_map_to_df(conn=conn, top=top, mode=mode)
+
+if not (len(df_path)):
+    raise Exception('Query empty (probably due to input gamemode inexistency) !')
+
+file =open('external_data/maps/attributes/map_transfer/file.txt','w')
+for element in df_path.iterrows():
+    file.write(element[1][2][1:]+'\n')
+    file.write('For '+str(element[1][0])+' : /tp '+str(element[1][3])+'\n')
+file.close()
