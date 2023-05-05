f978d65cc40e5c74cd53746e4b73ca4efca42330
niels <niels.tilch.1@gmail.com>
2023-05-05 12:00:54 +0200

    Map transfer menu

---
+++ src/external_data/maps/attributes/map_transfer/map_transfer_menu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Welcome to the transfer menu"
+
+echo "1- Specific transfer of a map"
+echo "2- Batch of top maps transfer"
+echo "3- Reset map file"
+
+read -p "Choice of transfer" choice
+
+if [ $choice = '1' ];then
+  cd ./top_map
+  bash top_map_transfer.sh
+fi
+
+if [ $choice = '2' ];then
+  cd ./specific_map
+  bash specific_map_transfer
+fi
+
+if [ $choice = '3' ];then
+  cd C:\Users
+  mc_file=$(find -path './*/AppData/Roaming/.minecraft' ! -path './Default/*' -print -quit )
+  mc_file="${mc_file:1}"
+  mc_path=$(pwd)$mc_file
+  cd $mc_path
+  rm -r saves
+  mkdir saves
+fi
